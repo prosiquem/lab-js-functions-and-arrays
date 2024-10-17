8e07ca4086d27fa6c47b2600b6e54538e28bce7c
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-10-18 00:22:51 +0200

    almost solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+
+function maxOfTwoNumbers(value1, value2) {
+    if (value1 > value2) {
+        return value1;
+    } else if (value2 > value1) {
+        return value2;
+    } else if (value1 === value2) {
+        return value1, value2;
+    }
+
+}
+
 
 
 
@@ -7,7 +18,25 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    for (let i = 0; i < words.length; i++);
+    if (words.length === 1) {
+        return words[0]
+    }
+
+    else if (words.length === 0) {
+        return null
+    }
+    longWord = " ";
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longWord.length) {
+            longWord = words[i]
+        }
+    }
+    return longWord
+}
+
+
 
 
 
@@ -15,20 +44,39 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    if (numbers.length === 0) {
+        return 0
+    }
+    else if (numbers.length === 1) {
+        return numbers[0]
+    }
 
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0
+    }
 
+    else if (numbers2.length === 1) {
+        return numbers2[0]
+    }
 
+}
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist() { }
